6610d75976c77fb832dca901b81d81488f713025
Wilbort Encomenderos <wilbort_enru@hotmail.com>
2021-10-27 22:38:06 -0500

    seccion seguridad

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 import { handleActiveMenu } from "./modules/menu.js";
 import { activePanel } from "./modules/active-panel.js";
+import { securityPanelScroll } from "./modules/padlock-animate.js";
 
 handleActiveMenu();
 activePanel();
+securityPanelScroll();
