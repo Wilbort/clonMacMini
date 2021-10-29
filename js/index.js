5a034b4d017df17e04c9ad3e1e94a000314a1e70
Wilbort Encomenderos <wilbort_enru@hotmail.com>
2021-10-28 23:53:12 -0500

    seccion tabs

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import { handleActiveMenu } from "./modules/menu.js";
 import { activePanel } from "./modules/active-panel.js";
 import { securityPanelScroll } from "./modules/padlock-animate.js";
+import { handleActiveTabs } from "./modules/tabs.js";
 
 handleActiveMenu();
 activePanel();
 securityPanelScroll();
+handleActiveTabs();
